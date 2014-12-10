92541973afca21473ed36380c539e3cfb8a7ee06
Hannes Sachsenhofer <hannes.sachsenhofer@epunkt.net>
2014-12-10 11:52:18 +0100

    Added script to extract a zip file.

---
+++ tools/Extract-Zip.ps1
@@ -0,0 +1,19 @@
+function Extract-Zip
+{
+    param(
+        [parameter(mandatory=$true)] [string] $file,
+        # default value for the destination directory is the script path and the name of the ZIP file
+        [string] $destination = (Join-Path $PSScriptRoot ([System.IO.Path]::GetFileNameWithoutExtension($file)))
+    )
+
+    # make sure the destination directory exists
+    New-Item -ItemType Directory -Force -Path $destination
+
+    $shell = new-object -com shell.application
+    $zip = $shell.NameSpace($file)
+
+    foreach($item in $zip.items())
+    {
+        $shell.Namespace($destination).copyhere($item)
+    }
+}
