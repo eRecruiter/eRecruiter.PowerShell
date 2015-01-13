91c9e948b2bc446f8ddf24348256d5b36b2853dd
Hannes Sachsenhofer <hannes.sachsenhofer@epunkt.net>
2015-01-13 10:47:22 +0100

    Better eR install script.

--- erecruiter-setup/Install-eRecruiter.ps1
+++ erecruiter-setup/Install-eRecruiter.ps1
@@ -1,45 +1,48 @@
+# copied from https://github.com/eRecruiter/eRecruiter.PowerShell
+
 # TODO : The following steps are not covered by this automatic routine (yet)
 # - Install certificates
 # - Set user rights to application directories
 
-
 # The name of the customer, to enable more than one eRecruiter installation on a single server
 # No spaces or other special characters please
-$customerName = "Customer_Name"
-
-# The URL to the ZIP package that contains the application source
-$sourceUrl = "http://staging.epunkt.net/eR_Without_Customization.zip"
-
-# The directory to install the applications to
-# WARNING: All existing files in this directory will be deleted!
-$installationDirectory = "c:\Applications\$customerName"
-
-# Whether or not to install the FileServer (PDF & video) service and CronWorker (maintainance) task
-$includeFileServer = $false
-$includeCronWorker = $false
+$customerName = "OurCustomer"
+$customerDomainName = "our-customer"
 
 # The windows user credentials
 # These are used to run the IIS applications and the CronWorker windows task
-$username = "DOMAIN\User"
-$password = "p4ssWord!"
+$username = "eR-$customerName@epunktweb.net"
+$password = "p4ssw0rd!!"
+
+# The URL to the ZIP package that contains the application source
+$sourceUrl = "http://staging.epunkt.net/eRecruiter.zip"
+
+# The directory to install the applications to
+# WARNING: All existing files in this directory will be deleted!
+$installationDirectory = "c:\Applications\eRecruiter\$customerName"
+
+# Whether or not to install the FileServer (PDF & video) service and CronWorker (maintenance) task
+$includeFileServer = $false
+$includeCronWorker = $true
 
 # The eRecruiter settings
-$eRecruiter_ConnectionString = "Data Source=.\;Initial Catalog=$customerName-eRecruiter;Integrated Security=True;"
-$eRecruiter_FilesDirectory = "c:\Data\Files"
-$eRecruiter_TemplatesDirectory = "c:\Data\Templates"
+$eRecruiter_ConnectionString = "Data Source=DB_SERVER,41433;Initial Catalog=eRecruiter-$customerName;Integrated Security=True;"
+$eRecruiter_FilesDirectory = "\\FILE_SERVER\$customerName\Files"
+$eRecruiter_TemplatesDirectory = "\\FILE_SERVER\$customerName\Templates"
 
 
 
 # This function is called down below, but I moved it up here because it contains a lot of configuration
-# And it would be cumbersome to put all this configuration into dozenns of variables
+# And it would be cumbersome to put all this configuration into dozens of variables
 function Configure-IIS-Bindings() {
-    $cert = "88 AA AA AA AA AA BB BB BB DE AD 87 6d d9 c5 d5 96 6a f0 ff".Replace(" ", "").ToUpper()
+    $erecruiter_cert = "cc 05 5a 1b ca 32 31 e1 c6 b8 6f cb 32 7a ee f9 76 77 fd a4".Replace(" ", "").ToUpper()
+    $bewerberportal_cert = "a2 af cc dd b8 7d 5b 0c 16 1f 1c 07 d6 95 e0 7d 0c 79 bf 2b".Replace(" ", "").ToUpper()	
+	$kandidatenportal_cert = "‎f9 66 4c 45 78 2d 8b 97 95 5a f7 b0 2c d6 5d 27 5c d6 f0 f8".Replace(" ", "").ToUpper()	
 
-    Set-Http-And-Https-Binding "$customerName-eRecruiter" "10.1.1.1" "$customerName.erecruiter.net" $cert
-    Set-Http-And-Https-Binding "$customerName-Api" "10.1.1.1" "$customerName-api.erecruiter.net" $cert
-    Set-Http-And-Https-Binding "$customerName-Portal" "10.1.1.2" "$customerName.kandidatenportal.eu" $cert
-    Set-Http-And-Https-Binding "$customerName-Responsive-Portal" "10.1.1.2" "$customerName-responsive.kandidatenportal.eu" $cert
-    Set-Http-And-Https-Binding "$customerName-CompanyPortal" "10.1.1.1" "$customerName-portal.erecruiter.net" $cert
+    Set-Http-And-Https-Binding "$customerName-eRecruiter" "194.9.58.162" "$customerDomainName.erecruiter.net" $erecruiter_cert
+    Set-Http-And-Https-Binding "$customerName-Api" "194.9.58.162" "$customerDomainName-api.erecruiter.net" $erecruiter_cert
+    Set-Http-And-Https-Binding "$customerName-Responsive-Portal" "194.9.58.163" "$customerDomainName.bewerberportal.at" $bewerberportal_cert
+    Set-Http-And-Https-Binding "$customerName-CompanyPortal" "194.9.58.162" "$customerDomainName-portal.erecruiter.net" $erecruiter_cert
 }
 
 
@@ -51,11 +54,10 @@ function Configure-eRecruiter-Settings() {
     Set-AppSetting-Recursive "RequireSsl" "true"
 
     Set-AppSetting-Recursive "Portal-MandatorId" "1" ".\Portal"
-
     Set-AppSetting-Recursive "MandatorId" "1" ".\Responsive-Portal"
-    Set-AppSetting-Recursive "ApiEndpoint" "some_url" ".\Responsive-Portal"
-    Set-AppSetting-Recursive "ApiKey" "some_key" ".\Responsive-Portal"
-    Set-AppSetting-Recursive "CustomFolder_$customerName-responsive.kandidatenportal.eu" "$customerName" ".\Responsive-Portal"
+    Set-AppSetting-Recursive "ApiEndpoint" "https://$customerDomainName-api.erecruiter.net/api" ".\Responsive-Portal"
+    Set-AppSetting-Recursive "ApiKey" "c066c02c3da149a18e6e9651008615687861c75be78342abba55f5b27a1efb0b21853bebbb974b158b292a702794f1f7" ".\Responsive-Portal"
+    Set-AppSetting-Recursive "CustomFolder_$customerDomainName.bewerberportal.at" "$customerName" ".\Responsive-Portal"
 }
 
 
@@ -74,7 +76,7 @@ Check-Credentials-And-Exit-If-Necessary $username $password
 
 Write-Host "Installing necessary IIS/ASP.NET modules ..."
 Import-Module ServerManager
-# this also installs a lot of dependencies
+# this also installs a lot of required dependencies (like IIS itself)
 Add-WindowsFeature -Name Web-Asp-Net45,Web-Mgmt-Console,Web-Dir-Browsing,Web-Http-Errors,Web-Static-Content,Web-Http-Logging,Web-Stat-Compression | Out-Null
 
 
@@ -83,10 +85,9 @@ Write-Host "Downloading the source ZIP package ..."
 $tempPackageFile = Join-Path $installationDirectory "temp_source.zip"
 Invoke-WebRequest $sourceUrl -OutFile $tempPackageFile
 if ((Test-Path $tempPackageFile) -eq $false) {
-    Write-Host "Download failed, make your your internet connection is working. Exiting ..." -ForegroundColor Yellow
+    Write-Host "Download from $sourceUrl failed, make your your internet connection is working. Exiting ..." -ForegroundColor Yellow
     exit -3
 }
-
 
 
 
@@ -103,6 +104,14 @@ if (Test-Path $tempPackageDirectory) {
 }
 iex (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/eRecruiter/eRecruiter.PowerShell/master/tools/Extract-Zip.ps1")
 Extract-Zip $tempPackageFile $tempPackageDirectory
+# if there is a root "eRecruiter" directory in the extracted folder, remove it
+$tempPackageRootDirectory = Join-Path $tempPackageDirectory "eRecruiter"
+if (Test-Path $tempPackageRootDirectory) {
+	Rename-Item $tempPackageRootDirectory "tmp"
+	$tempPackageRootDirectory = Join-Path $tempPackageDirectory "tmp"
+	Move-Item $tempPackageRootDirectory\* $tempPackageDirectory
+	Remove-Item -Force $tempPackageRootDirectory
+}
 
 
 
@@ -232,6 +241,6 @@ Write-Host "Removing temporary files ..."
 Remove-Item -Recurse -Force $tempPackageFile | Out-Null
 Remove-Item -Recurse -Force $tempPackageDirectory | Out-Null
 
-
-
+# go back one directory, to end where we started
+cd..
 Write-Host "Everything done. Have a nice day."
